9d1d0bd5db41420edb02f93a8c018a858a6c2319
Vasiliy Yorkin <vasiliy.yorkin@gmail.com>
2023-12-13 16:28:36 +0300

    parse lists

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,42 @@ use std::env;
 // Available if you need it!
 // use serde_bencode
 
-fn decode_bencoded_value(encoded_value: &str) -> Result<serde_json::Value, String> {
-    if let Some(n) = encoded_value
-        .strip_prefix('i')
-        .and_then(|rest| rest.strip_suffix('e'))
-        .and_then(|s| s.parse::<i64>().ok())
-    {
-        return Ok(n.into());
-    } else if let Some(len) = encoded_value
-        .split_once(':')
-        .and_then(|(len, rest)| len.parse::<usize>().ok().map(|len| rest[..len].to_string()))
-    {
-        return Ok(len.into());
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, &str) {
+    let (tag, mut rest) = encoded_value.split_at(1);
+    let tag_char = tag.chars().next().expect("Tag doesn't exist");
+    match tag_char {
+        'i' => {
+            // i52e
+            if let Some((s, remainder)) = rest.split_once('e') {
+                if let Ok(n) = s.parse::<i64>() {
+                    return (n.into(), remainder);
+                }
+            }
+        }
+        'l' => {
+            // l<bencoded_elements>e
+            // l5:helloi52ee
+            let mut values = Vec::new();
+            while !rest.is_empty() && !rest.starts_with('e') {
+                let (v, remainder) = decode_bencoded_value(rest);
+                rest = remainder;
+                values.push(v);
+            }
+            return (values.into(), &rest[1..]);
+        }
+        '0'..='9' => {
+            if let Some((str, rest)) = rest.split_once(':').and_then(|(_, remainder)| {
+                tag.parse::<usize>()
+                    .ok()
+                    .map(|len| (remainder[..len].to_string(), &remainder[len..]))
+            }) {
+                return (str.into(), rest);
+            }
+        }
+        _ => {}
     }
-    Err(format!("Unhandled encoded value: {}", encoded_value))
+
+    panic!("unhandled encoded value: {}", encoded_value);
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
@@ -26,10 +48,8 @@ fn main() {
 
     if command == "decode" {
         let encoded_value = &args[2];
-        match decode_bencoded_value(encoded_value) {
-            Ok(decoded_value) => println!("{}", decoded_value),
-            Err(err) => eprintln!("error: {}", err),
-        }
+        let (decoded_value, _) = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value);
     } else {
         println!("unknown command: {}", args[1])
     }
